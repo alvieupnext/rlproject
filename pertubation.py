5183072a903368d835f47fdfc6ae0c3d053dac73
alvieupnext <alvarovargasguerrero@hotmail.com>
2024-03-30 14:22:14 +0100

    removed default sigma value

--- pertubation.py
+++ pertubation.py
@@ -24,7 +24,7 @@ import copy
 
 #Write a function that given a policy, will produce N perturbations of θ, with a perturbation strength of σ.
 # This function should receive a parametric policy and return a list of perturbed policies.
-def perturb_policy_zeroth_order(original_policy, sigma=0.1):
+def perturb_policy_zeroth_order(original_policy, sigma):
 
   # Get the parameters of the original policy
   original_parameters = [p.data for p in original_policy.parameters()]
